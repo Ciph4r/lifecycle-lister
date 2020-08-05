6bb19c64de15d3a3f4ead42477e94521c9d11c4d
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-08-05 15:51:40 -0400

    finish

--- src/component/Albums.js
+++ src/component/Albums.js
@@ -11,7 +11,7 @@ const Albums = (props) => {
                     <div key ={album.id}>
                         <h1> Title: {album.title}</h1>
                         <h1><a href= './'>Url: https://jsonplaceholder.typicode.com/albums/{album.id}</a> </h1>
-                        <img alt = '...' src = {album.url} /> 
+                        <img alt = '...' src = {album.thumbnailUrl} /> 
                     </div>
            )
             })}
